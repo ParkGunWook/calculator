e18f8779c5b5b88a51b8750263bb0be914ea818b
ParkGunWook <saiver852@gmail.com>
2022-01-24 17:41:28 +0900

    충돌을 일으키겠다.

--- main-13.c
+++ main-13.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	int numberone, numbertwo, result;
 	char operator;
 	
-	printf("Please enter a expression :;;:");
+	printf("Please enter a expression ex) 1 + 2 : ");
 	scanf("%d %c %d", &numberone, &operator, &numbertwo);
 	
 	switch (operator)
@@ -41,7 +41,7 @@ int add(int numberone, int numbertwo)
  
 int sub(int numberone, int numbertwo)
 {
-	return numbrerone - numbertwo;
+	return numberone - numbertwo;
 }
  
 int multi(int numberone, int numbertwo)
